584811c8e9685361cd4fee0c180a93dec84b539d
chanqihui <91418311+chanqihui@users.noreply.github.com>
2022-01-07 16:09:45 +1000

    feat: connect db and create table
    Connect mysql database and create table users

--- user_upload.php
+++ user_upload.php
@@ -1,5 +1,53 @@
-<?php 
-/*
-A test from Catalyst.
-*/
-?>
+<?php
+
+function create_db()
+{
+    $servername = "localhost";
+    $username = "catalyst";
+    $password = "qwert123Aa";
+    //$dbname = "catalystDB";
+
+    // Create connection
+    $conn = mysqli_connect($servername, $username, $password);
+
+    // Check connection
+    if (!$conn) {
+        die("Connection failed:\n" . mysqli_connect_error() . "\n");
+    }
+    echo "Connected MySQL successfully\n";
+
+    // Create database
+    $sql = "CREATE DATABASE if not exists catalystDB";
+    if ($conn->query($sql) === TRUE) {
+        $conn->select_db("catalystDB");
+        echo "Database created successfully\n";
+    } else {
+        echo "Error creating database: " . $conn->error . "\n";
+    }
+
+    return $conn;
+}
+
+function create_tb($conn)
+{
+    $sql = "CREATE TABLE users (
+        id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(30) NOT NULL,
+        surname VARCHAR(30) NOT NULL,
+        email VARCHAR(50) Unique
+        );";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Table users created successfully\n";
+    } else {
+        echo "Error creating table: " . $conn->error . "\n";
+    }
+}
+
+function main()
+{
+    $conn = create_db();
+    create_tb($conn);
+}
+
+main();
